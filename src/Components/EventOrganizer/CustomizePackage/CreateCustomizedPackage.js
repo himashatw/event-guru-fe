bc1bc535a96dfe346c7ac8fb02cb0d7aa6509eee
ushan1997 <ushananuka5@gmail.com>
2021-08-20 23:02:43 +0530

    create customize pakage function

---
+++ src/Components/EventOrganizer/CustomizePackage/CreateCustomizedPackage.js
@@ -0,0 +1,8 @@
+import axios from '../../services/axios';
+import React, { useState } from "react";
+
+const CreateCustomizedPackage = () => {
+    const []
+
+}
+export default CreateCustomizedPackage;
