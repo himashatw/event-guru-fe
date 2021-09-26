396c8034a89533b1306ecbf058d9c62468b7bb37
Himasha Weerasinghe <himashatw@gmail.com>
2021-09-27 00:54:37 +0530

    dashboard done

--- src/Components/Admin/AdminDashboard.js
+++ src/Components/Admin/AdminDashboard.js
@@ -52,6 +52,7 @@ function AdminDashboard(props) {
                   <button
                     type="button"
                     class="border border-green-500 text-green-500 rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:text-white hover:bg-green-600 focus:outline-none focus:shadow-outline"
+                    onClick={() => history.push("/admin/manageregistrations")}
                   >
                     Click
                   </button>
